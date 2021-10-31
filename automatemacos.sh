ebdddc4674249c6e3943bc034e94cdb3751587ad
Heikki Lavanko <noobtron@gmail.com>
2021-10-31 12:00:58 +0200

    macos changes

--- automatemacos.sh
+++ automatemacos.sh
@@ -1,3 +1,3 @@
 ip="$(curl ifconfig.me)"
-aws ec2 create-key-pair --key-name mb-key --query 'KeyMaterial' --output text | out-file -encoding ascii -filepath mbkey.pem
+aws ec2 create-key-pair --key-name mb-key --query 'KeyMaterial' --output text > mbkey.pem
 aws cloudformation create-stack --stack-name minecraft-bedrock-server --template-body file://cf-main.yaml --parameters ParameterKey=WorkstationIp,ParameterValue=$ip ParameterKey=Key,ParameterValue=mb-key
